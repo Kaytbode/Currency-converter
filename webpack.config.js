fd30c0abf79406e48e4f53d3c85625848e76a2a9
Kayode <bkaoluborode@gmail.com>
2018-06-28 13:11:06 +0100

    modified webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './docs/src/app.js',
   devServer: {
-    contentBase: path.join(__dirname, 'dist'),
+    contentBase: path.join(__dirname, 'docs'),
     compress: true,
     port: 9000
   }
